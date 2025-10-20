d07c4e48c9d93b94d3e31e3c06e2ff0c394dca8a
Joyi <joyi.x.06@gmail.com>
2025-10-20 12:01:26 -0400

    LogoutController
    LogoutController both todos should be implemented

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,8 @@
 package interface_adapter.logout;
 
+import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutOutputData;
 
 /**
  * The controller for the Logout Use Case.
@@ -11,6 +13,7 @@ public class LogoutController {
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
         // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
@@ -18,5 +21,7 @@ public class LogoutController {
      */
     public void execute() {
         // TODO: run the use case interactor for the logout use case
+        // final LogoutOutputData logoutOutputData = new LogoutOutputData(username);
+        logoutUseCaseInteractor.execute();
     }
 }
